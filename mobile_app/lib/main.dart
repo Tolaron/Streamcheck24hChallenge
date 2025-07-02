63e28d4fb5ebb9880b3143c7e2939e7ce376587f
PC-GRISEO\Griseo <r_beck1987@hotmail.com>
2025-07-02 09:04:21 +0200

    Setup Flutter project with HTTP client and routing (in android Studio)

---
+++ mobile_app/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
+import 'screens/event_screen.dart';
+
+void main() {
+  runApp(const EventApp());
+}
+
+class EventApp extends StatelessWidget {
+  const EventApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Event App',
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const HomeScreen(),
+        '/event': (context) => const EventScreen(),
+      },
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+    );
+  }
+}
