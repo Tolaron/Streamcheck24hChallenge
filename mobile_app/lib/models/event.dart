2cc7fe3622e829c475ee771ce8ce91df833d0948
PC-GRISEO\Griseo <r_beck1987@hotmail.com>
2025-07-02 11:16:12 +0200

    updated Event model and EventService for backend API integration -> not enough time to finish it

--- mobile_app/lib/models/event.dart
+++ mobile_app/lib/models/event.dart
@@ -14,4 +14,25 @@ class Event
     required this.date,
     required this.location,
   });
+
+  // JSON → Event
+  factory Event.fromJson(Map<String, dynamic> json) {
+    return Event(
+      id: json['id'].toString(), // ID als String erzwingen
+      title: json['title'],
+      description: json['description'],
+      date: DateTime.parse(json['date']),
+      location: json['location'],
+    );
+  }
+
+  // Event → JSON (für create)
+  Map<String, dynamic> toJson() {
+    return {
+      'title': title,
+      'description': description,
+      'date': date.toIso8601String(),
+      'location': location,
+    };
+  }
 }
