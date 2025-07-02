b0a4b899a7ba80af4beaf75c704d9fa118bc5c4a
PC-GRISEO\Griseo <r_beck1987@hotmail.com>
2025-07-02 10:21:51 +0200

    added bottom navigation with tab bar for EventList, CreateEvent, and Profile screens

---
+++ mobile_app/lib/screens/main_navigation_screen.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'event_list_screen.dart';
+import 'create_event_screen.dart';
+import 'profile_screen.dart';
+
+class MainNavigationScreen extends StatefulWidget {
+  const MainNavigationScreen({super.key});
+
+  @override
+  State<MainNavigationScreen> createState() => _MainNavigationScreenState();
+}
+
+class _MainNavigationScreenState extends State<MainNavigationScreen> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _screens = const [
+    EventListScreen(),
+    CreateEventScreen(),
+    ProfileScreen(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.event),
+            label: 'Events',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add),
+            label: 'Erstellen',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profil',
+          ),
+        ],
+      ),
+    );
+  }
+}
